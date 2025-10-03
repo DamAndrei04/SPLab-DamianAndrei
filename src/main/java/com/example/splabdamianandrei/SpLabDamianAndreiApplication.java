ccc4895255df3dd03088682e03c284b81cf19ffd
DamAndrei04 <andrei.damian04@e-uvt.ro>
2025-10-03 10:17:47 +0300

    Initial commit

---
+++ src/main/java/com/example/splabdamianandrei/SpLabDamianAndreiApplication.java
@@ -0,0 +1,13 @@
+package com.example.splabdamianandrei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLabDamianAndreiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLabDamianAndreiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/splabdamianandrei/SpLabDamianAndreiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splabdamianandrei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpLabDamianAndreiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
