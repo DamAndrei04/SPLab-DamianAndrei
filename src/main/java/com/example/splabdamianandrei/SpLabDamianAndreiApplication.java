1aba0fa43d8b1485ae7374e8d35250d3984e69fd
DamAndrei04 <andrei.damian04@e-uvt.ro>
2025-11-27 00:41:35 +0200

    Lab 7

--- src/main/java/com/example/splabdamianandrei/SpLabDamianAndreiApplication.java
+++ src/main/java/com/example/splabdamianandrei/SpLabDamianAndreiApplication.java
@@ -1,57 +1,26 @@
 package com.example.splabdamianandrei;
 
-import com.example.splabdamianandrei.components.ClientComponent;
-import com.example.splabdamianandrei.components.SingletonComponent;
-import com.example.splabdamianandrei.components.TransientComponent;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpLabDamianAndreiApplication {
 
-    public static void main(String[] args) throws Exception {
-        ApplicationContext context = SpringApplication.run(SpLabDamianAndreiApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(SpLabDamianAndreiApplication.class, args);
+    }
 
-        TransientComponent transientBean = context.getBean(TransientComponent.class);
-        transientBean.operation();
-        transientBean = context.getBean(TransientComponent.class);
-        transientBean.operation();
-
-        SingletonComponent singletonBean = context.getBean(SingletonComponent.class);
-        singletonBean.operation();
-        singletonBean = context.getBean(SingletonComponent.class);
-        singletonBean.operation();
-
-        ClientComponent c = context.getBean(ClientComponent.class);
-        c.operation();
-
-        c = (ClientComponent) context.getBean("clientComponent");
-        c.operation();
-
-
-
-
-/*
-        Section cap1 = new Section("Capitolul 1");
-        Paragraph p1 = new Paragraph("Paragraph 1");
-        cap1.add(p1);
-        Paragraph p2 = new Paragraph("Paragraph 2");
-        cap1.add(p2);
-        Paragraph p3 = new Paragraph("Paragraph 3");
-        cap1.add(p3);
-        Paragraph p4 = new Paragraph("Paragraph 4");
-        cap1.add(p4);
-        System.out.println("Printing without Alignment");
-        System.out.println();
-        cap1.print();
-        p1.setAlignStrategy(new AlignCenter());
-        p2.setAlignStrategy(new AlignRight());
-        p3.setAlignStrategy(new AlignLeft());
-
-        System.out.println();
-        System.out.println("Printing with Alignment");
-        System.out.println();
-        cap1.print();*/
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
